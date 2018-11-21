a94be6a68120867827fb7754df3e66868811bfa1
Mark Villudo <mark.villudo@synergy88digital.com>
2018-11-21 12:14:12 +0800

    Initial commit for ecommerce plugins

---
+++ src/MarkVilludo/LaravelEcommerce/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace Mannysoft\Hanap;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Mannysoft\Hanap\Hanap;
+
+class ServiceProvider extends BaseServiceProvider {
+    
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+       
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+     
+    }
+
+}
---
+++ src/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists('test')) {
+    /**
+     * Get the Hanap instance
+     *
+     * @return \Mannysoft\Hanap\Hanap
+     */
+    function test()
+    {
+        return 'test';
+    }
+}
